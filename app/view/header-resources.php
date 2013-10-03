e48555cc99927fd45d559d11997ce67a8eda8f6c
mwyatt <martin,wyatt@gmail.com>
2013-10-04 00:48:04 +0100

    league draw

--- app/view/header-resources.php
+++ app/view/header-resources.php
@@ -4,8 +4,6 @@
 <meta name="keywords" content="<?php echo $this->getMeta('keywords'); ?>">
 <meta name="description" content="<?php echo $this->getMeta('description'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="stylesheet" type="text/css" href="<?php echo $this->urlHome(); ?>css/main-v003.css?rev=2">
-<!-- <link rel="stylesheet/less" type="text/css" href="<?php echo $this->urlHome(); ?>css/main.less">
-<script src="<?php echo $this->urlHome(); ?>js/vendor/less-1.3.3.min.js"></script> -->
+<link rel="stylesheet" type="text/css" href="<?php echo $this->urlHome(); ?>css/screen.css?rev=1.1">
 <script src="<?php echo $this->urlHome(); ?>js/vendor/respond.min.js"></script>
 <script src="<?php echo $this->urlHome(); ?>js/vendor/modernizr.custom.73218.js"></script>
