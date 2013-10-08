9e7081f6756359df149c6a81c363643a1f889ebc
mwyatt <martin,wyatt@gmail.com>
2013-10-08 20:38:00 +0100

    screen.css

--- app/view/header-resources.php
+++ app/view/header-resources.php
@@ -4,6 +4,6 @@
 <meta name="keywords" content="<?php echo $this->getMeta('keywords'); ?>">
 <meta name="description" content="<?php echo $this->getMeta('description'); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<link rel="stylesheet" type="text/css" href="<?php echo $this->urlHome(); ?>css/screen.css?rev=1.1">
+<link rel="stylesheet" type="text/css" href="<?php echo $this->urlHome(); ?>css/screen.css?rev=1.2">
 <script src="<?php echo $this->urlHome(); ?>js/vendor/respond.min.js"></script>
 <script src="<?php echo $this->urlHome(); ?>js/vendor/modernizr.custom.73218.js"></script>
